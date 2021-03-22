cd3c82e6558519641138c1edd94f1e7bd2d33709
MuskanMandloi <54906865+muskanmandloi@users.noreply.github.com>
2021-03-22 15:48:57 +0530

    Add files via upload
    add

---
+++ division.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    double a, b, divide,exponent,e1,e2;
+    printf("Enter two numbers: ");
+    scanf("%lf %lf", &a, &b);  
+ 
+    
+   divide = a / b;
+   exponent=e1-e2;
+    
+    printf("divide = %.2lf",divide,exponent);
+    
+    return 0;
+}
---
+++ multiplication.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    double a, b, product,exponent,e1,e2;
+    printf("Enter two numbers with their exponents: ");
+    scanf("%lf %lf", &a, &b);  
+ 
+    
+    product = a * b;
+    exponent=e1+e2;
+    
+    printf("Product = %.2lf", product,exponent);
+    
+    return 0;
+}
